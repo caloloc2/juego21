ad139abd68eb696001d2adfe70d90805d60ab073
caloloc2 <calolomino@gmail.com>
2020-09-09 23:49:02 -0500

    upload

---
+++ README.md
@@ -0,0 +1,23 @@
+El casino virtual en el que entrará el usuario llevará el nombre de Helsinki, el programa contendrá todos los elementos de un casino, empezando por una bienvenida cordial que se le dará al ingreso, además, le entregará una tarjeta de recargo, en dicha tarjeta el cliente deberá depositar el dinero que está dispuesto a gastar al interior del lugar. En todas las rondas, el jugador elegirá la cantidad que desea apostar y se irá descontando del valor de esta tarjeta, en caso de ganar el dinero se recargará en la misma. De esta manera, se asegura la disponibilidad de dinero antes de aceptar una apuesta o comenzar el juego.  
+
+Luego de entregarle la tarjeta, se le designará una mesa para jugar “Veintiuno” en la que se encontrará un crupier designado por el casino, quién será el encargado de informarle las reglas del juego, encargarse del cumplimiento de estas y jugar junto al participante. Dentro de estas reglas se debe aclarar el valor de la apuesta mínima y máxima, el objetivo que es sumar 21 con las cartas, lo que sucede en caso de que no llegue a 21 con las dos cartas, indicar que el jugador puede solicitar una carta más al crupier según sea conveniente para el mismo. Asimismo, notificarle que el juego termina cuando cualquiera de los jugadores llega a 21 o se sobrepasa, si es mayor o menor a 21, gana la persona cuya suma de cartas se acerque más a 21 sin pasarse. 
+
+Antes de empezar cualquier ronda se le informa al jugador el dinero del que dispone la tarjeta y se le solicita que ingrese el valor de su apuesta, en cuanto a los beneficios del casino se ha establecido que el valor de la apuesta mínima sea 100 dólares y la máxima en cada ronda sea 1000 dólares, en caso de que el jugador elija un valor menor  a 100, el crupier restará de su tarjeta la tarifa mínima de 100 dólares, en caso de ser un valor mayor el crupier restará de su tarjeta la tarifa máxima de 1000 dólares. 
+
+Después de que el usuario ingrese su apuesta se le repartirá a él y al crupier dos cartas aleatorias y se indicará el valor de la suma de estas, el jugador decidirá si desea aumentar su apuesta y a su vez si requiere una carta más, se le hará esta pregunta antes de entregarle otra carta.
+
+En el juego se le dan varias oportunidades para ganar al usuario, Dado que la carta sea un A el crupier le pregunta al cliente si le desea dar un valor de 11 o 1 y si vuelve a salir un A esta ya tomará directamente el valor elegido. Así mismo, si llega a salirle al cliente un 7 podrá cambiar una de sus cartas. Cuando el jugador tenga tres o más cartas y en ellas estén un A junto a un 4 podrá cambiar el valor del A ingresado al principio y se volverá a calcular la suma.  
+
+En caso de no requerir más cartas se puede entregar una ronda más para el crupier y se presentan las sumas de ambos, gana el que esté más cercano a 21. En caso de que el jugador obtenga la suma de 21 ganará el doble de su apuesta. Al finalizar este proceso el crupier indica la cantidad ganada o perdida. 
+
+El usuario elegirá si desea jugar otra ronda o retirarse. Cuando el cliente decida retirarse del casino, deberá entregar su tarjeta y se le devolverá el dinero que esta contenga.
+
+REQUERIMIENTOS MINIMOS DEL PROYECTO
+
+1. Sera implementado con varias funciones (minimo 2 sin contar con la main)
+2. Se debe implementar aletoriedad al menos para algun proceso a ser utilizado en el proyecto
+3. Debe haber al menos un menu de seleccion multiple
+4. Se debe implementar de alguna forma la visualizacion de una explicacion del programa, como usarlo, que requerimientos necesitamos, como jugar, reglas del juego, etc.
+5. Se debe implementar los tres tipos de estructura de control.
+6. Se apreciara que se utilicen diferentes tipos de estructuras y anidamientos.
+7. Se apreciara el uso de vectores, arreglos y archivos si el juego/programa lo amerita
---
+++ juego21.cpp
@@ -0,0 +1,359 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <windows.h>
+
+/* DECLARACION DE VARIABLES, VECTORES Y ARREGLOS */
+int opcion;
+float saldo_actual=0;
+float limites[2] = {100, 1000};
+int jugador[10];
+int cuprier[10];
+int numero_cartas = 0;
+float valor_apostado = 0 ;
+float apuesta = 0;
+bool cambio_as = false;
+int valor_as = 1;
+
+/* DECLARACION DE FUNCIONES */
+float saldo_tarjeta(void);
+void reglas(void);
+int aleatorio(int, int);
+float obtener_saldo(void);
+void modificar_saldo(float, int);
+int suma_cartas(int);
+void realizar_apuesta(void);
+int barajar(void);
+void mostrar_cartas(int);
+void analiza_cartas(void);
+void reinicia(void);
+void nombres_cartas(int);
+
+/* FUNCION PRINCIPAL */
+int main(){	
+    system("cls");
+    
+    printf("Te saluda Helsinki, te doy la bienvenida al casino\n");
+    printf("Por el momento solo disponemos mesas para BlackJack\n\n");
+    
+    printf("Su mesa asignada es la %d\n", aleatorio(1, 10));
+    printf("Su numero de tarjeta es %d\n\n", aleatorio(100, 200));
+
+    while(opcion!=5){
+    	printf("Selecciona una opción:\n");
+	    printf("1. Ver reglas del juego\n");
+	    printf("2. Empezar el juego\n");
+	    printf("3. Ver saldo de tarjeta\n");
+	    printf("4. Cargar saldo a tarjeta\n");
+	    printf("5. Salir\n");
+    
+    	printf("Su opción: ");
+    	scanf("%d", &opcion);    	
+    	
+    	switch(opcion){
+			case 1: {
+				system("cls");
+				reglas();
+				break;
+			}
+			case 2: {
+				system("cls");
+				printf("EMPIEZA EL JUEGO\n\n");
+				int op2 = 0;
+				bool valida = true;
+				bool juega = true;
+				
+				while(juega){
+					if (saldo_tarjeta()>0){
+						printf("Su saldo actual es de %f\n", saldo_tarjeta());
+					
+						realizar_apuesta(); /* Realiza primera apuesta */
+						
+						printf("Repartiendo cartas...\n\n");
+						/* SE REPARTE DOS CARTAS AL JUGADOR Y CUPRIER */
+						for (int x=0; x<2; x++){
+							jugador[x] = barajar();
+							cuprier[x] = barajar();
+							numero_cartas += 1;
+						}
+						
+						mostrar_cartas(0);					
+						
+						if ((suma_cartas(0)<21) and (suma_cartas(1)<21)){
+							while(valida){
+								printf("1. Aumentar apuesta\n");
+								printf("2. Otra carta\n");
+								printf("3. No mas cartas\n");
+								printf("Seleccione una opcion: ");
+								
+								scanf("%d", &op2);
+								
+								switch(op2){
+									case 1: {								
+										realizar_apuesta(); /* Realiza nueva apuesta */									
+										break;
+									};
+									case 2: {
+										printf("\nRepartiendo cartas al jugador...\n\n");
+										int nueva_carta = barajar();										
+										
+										if (nueva_carta==1){
+											if (cambio_as==false){											
+												int op3 = 0;
+												printf("Has obtenido un AS.\n");
+												printf("1. Cambiar valor a 1.\n");
+												printf("2. Cambiar valor a 11.\n");
+												
+												while((op3!=1)and(op3!=2)){
+													printf("Selecciona una opcion: ");
+													scanf("%d", &op3);
+												}
+												
+												if (op3==1){
+													valor_as = 1;
+												}else{
+													valor_as = 11;
+												}
+												
+												cambio_as = true;
+											}
+												
+											jugador[numero_cartas] = valor_as;
+											numero_cartas += 1;
+										}else if (nueva_carta==7){
+											int reemplazo=20;
+											printf("Has obtenido un 7.\n");
+											while(reemplazo>numero_cartas){												
+												printf("Escoja una carta a reemplazar: ");
+												scanf("%d", &reemplazo);
+												
+												jugador[(reemplazo-1)] = nueva_carta;												
+											}
+										}else{
+											jugador[numero_cartas] = barajar();
+											numero_cartas += 1;	
+										}
+																				
+										mostrar_cartas(0);										
+										
+										break;
+									};
+									case 3: {
+										printf("\nRepartiendo cartas al cuprier...\n\n");
+										cuprier[numero_cartas] = barajar();
+										numero_cartas += 1;
+										
+										mostrar_cartas(1);
+										
+										analiza_cartas();
+																				
+										printf("\nSu saldo actual es de %f\n\n\n", saldo_tarjeta());
+										
+										juega = false;
+										valida = false;										
+										reinicia();
+										break;
+									};
+									default: {
+										printf("Opcion invalida\n");
+										break;
+									}
+								}	
+							}	
+						}else{						
+							printf("Juego terminado\n");
+							juega = false;
+						}
+					}else{
+						printf("No tiene saldo en su tarjeta. Por favor recarguela.\n\n\n");
+						juega = false;
+					}
+				}				
+				break;
+			}
+			case 3: {	
+				system("cls");			
+				printf("\n\nSu saldo es de %f\n\n\n", saldo_tarjeta());
+				break;	
+			};
+			case 4: {
+				system("cls");
+				float valor = 0;
+				printf("\n\nValor a cargar: $ ");
+				scanf("%f", &valor);
+				modificar_saldo(valor, 0);
+				printf("Su nuevo saldo es de %f\n\n\n", saldo_tarjeta());
+				break;	
+			};
+			case 5: {
+				if (saldo_tarjeta()>0){
+					printf("\n\nSe le entrega $ %f USD.\n", saldo_tarjeta());
+				}else{
+					printf("\n\nNo ha ganado nada en el juego.\n");
+				}
+				printf("Adios.");
+				break;
+			};
+			default: {
+				printf("Opcion invalida\n");
+				break;
+			}
+		}
+	}
+}
+
+/* DEFINICION DE FUNCIONES */
+
+float saldo_tarjeta(void){
+	return saldo_actual;
+}
+
+void reglas(void){
+	printf("REGLAS DEL JUEGO\n\n");
+	printf("1. Su puesta minima es de %f y maxima de %f\n", limites[0], limites[1]);
+	printf("2. El juego consiste en sumar 21 con sus cartas\n");
+	printf("3. El juego termina cuando cualquiera de los jugadores llega a 21 o se sobrepasa, si es mayor o menos a 21, gana la persona cuya suma de cartas se acerque mas a 21 sin pasarse.\n\n\n");
+}
+
+int aleatorio(int min, int max){
+	srand(time(NULL));
+	return min + rand() % (max + 1 - min);
+}
+
+int barajar(void){	
+	int r_palo = aleatorio(1, 4);
+	int num_carta = aleatorio(1, 13);
+	Sleep(1000);
+	return num_carta;
+}
+
+float obtener_saldo(void){
+	return saldo_actual;
+}
+
+void modificar_saldo(float valor, int op){
+	if (op==0){
+		saldo_actual += valor;
+	}else{
+		saldo_actual -= valor;
+	}
+}
+
+int suma_cartas(int op){
+	int total = 0;
+	for (int x=0; x<10; x++){
+		if (op==0){
+			total += jugador[x];
+		}else{
+			total += cuprier[x];
+		}
+	}
+	return total;
+}
+
+void nombres_cartas(int carta){
+	switch(carta){
+		case 1: {
+			printf("AS");
+			break;
+		};		
+		case 11: {
+			printf("J");
+			break;
+		};
+		case 12: {
+			printf("Q");
+			break;
+		};
+		case 13: {
+			printf("K");
+			break;
+		};
+		default: {
+			printf("%d", carta);
+			break;
+		}
+	}
+}
+
+void mostrar_cartas(int op){	
+	printf("Cartas del jugador = [");
+	for (int x=0; x<numero_cartas; x++){
+		nombres_cartas(jugador[x]);
+		if (x<(numero_cartas-1)){
+			printf(" - ");
+		}
+	}
+	printf("]\n");
+	printf("La suma de tus cartas es de %d\n\n", suma_cartas(0));
+	
+	if (op==1){
+		printf("Cartas del cuprier = [");
+		for (int x=0; x<numero_cartas; x++){
+			nombres_cartas(cuprier[x]);			
+			if (x<(numero_cartas-1)){
+				printf(" - ");
+			}
+		}
+		printf("]\n");
+		printf("La suma de las cartas del cuprier es de %d\n\n", suma_cartas(1));	
+	}	
+}
+
+void realizar_apuesta(){
+	printf("\nIngrese el valor de su apuesta: $ ");
+	scanf("%f", &apuesta);
+	
+	if ((apuesta>=limites[0]) and (apuesta<=limites[1])){
+		printf("Se ha descontado $ %f de su tarjeta\n\n", apuesta);
+		modificar_saldo(apuesta, 1);
+		valor_apostado += apuesta;
+	}else if (apuesta<limites[0]){
+		printf("Se ha descontado $ %f de su tarjeta\n\n", limites[0]);
+		modificar_saldo(limites[0], 1);
+		valor_apostado += limites[0];
+	}else if (apuesta>limites[1]){
+		printf("Se ha descontado $ %f de su tarjeta\n\n", limites[1]);
+		modificar_saldo(limites[1], 1);
+		valor_apostado += limites[1];
+	}
+}
+
+void analiza_cartas(void){
+	float total_ganado = valor_apostado * 2;
+	int suma_jugador = suma_cartas(0);
+	int suma_cuprier = suma_cartas(1);
+		
+	if (suma_jugador>suma_cuprier){
+		if (suma_jugador<=21){
+			printf("\nUsted ha ganado $ %f USD!!\n", total_ganado);
+			modificar_saldo(total_ganado, 0);	
+		}else{
+			printf("\nCuprier ha ganado\n");
+			printf("Usted ha perdido $ %f USD!!\n", valor_apostado);
+		}		
+	}else if (suma_jugador<suma_cuprier){
+		if ((suma_cuprier==21) and (suma_jugador==21)){
+			printf("\nExiste un empate\n");
+			printf("Usted ha perdido $ %f USD!!\n", valor_apostado);
+		}else if ((suma_cuprier<=21) and (suma_jugador<21)){
+			printf("\nCuprier ha ganado\n");
+			printf("Usted ha perdido $ %f USD!!\n", valor_apostado);
+		}else if ((suma_cuprier>21) and (suma_jugador<=21)){			
+			printf("\nUsted ha ganado $ %f USD!!\n", total_ganado);
+			modificar_saldo(total_ganado, 0);
+		}else if ((suma_cuprier>21) and (suma_jugador>21)){
+			printf("\nNadie ha ganado\n");
+			printf("Usted ha perdido $ %f USD!!\n", valor_apostado);
+		}
+	}		
+}
+
+void reinicia(void){
+	numero_cartas = 0;
+	valor_apostado = 0;
+	for (int x=0; x<10; x++){		
+		jugador[x] = 0;
+		cuprier[x] = 0;
+	}
+}
